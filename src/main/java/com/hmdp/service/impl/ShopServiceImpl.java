2b4b11f9fe70db01e131461e75ec2795bbe71b3e
master <Welty898@gmail.com>
2025-10-07 20:14:46 +0800

    init

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+
 
 /**
  * <p>
@@ -17,4 +31,95 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+
+        Shop shop = queryWithPenetration(id);
+
+        return Result.ok(shop);
+    }
+
+    public Shop queryWithPenetration(Long id){
+        String key = CACHE_SHOP_KEY + id;
+
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        if (!StrUtil.isBlank(shopJson)) {
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+
+        if (shopJson != null){
+            return null;
+        }
+
+        Shop byId = getById(id);
+
+        if (byId == null) {
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+            return null;
+        }
+
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(byId),CACHE_SHOP_TTL,TimeUnit.MINUTES);
+
+        return byId;
+    }
+
+    public Shop queryWithBreakDown(Long id) throws InterruptedException {
+        String key = CACHE_SHOP_KEY + id;
+
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        if (!StrUtil.isBlank(shopJson)) {
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+
+        if (shopJson != null){
+            return null;
+        }
+
+        String lockKey = "lock:shop:" + id;
+
+        boolean triedLock = tryLock(lockKey);
+        if (!triedLock){
+            Thread.sleep(50);
+            return queryWithBreakDown(id);
+        }
+
+        Shop byId = getById(id);
+
+        if (byId == null) {
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+            return null;
+        }
+
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(byId),CACHE_SHOP_TTL,TimeUnit.MINUTES);
+
+        unLock(lockKey);
+
+        return byId;
+    }
+
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key,"1",10,TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    public void unLock(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+    @Override
+    public Result update(Shop shop) {
+        if (shop.getId() == null){
+            return Result.fail("THIS SHOP ISN'T EXIST , ID CAN'T BE NULL");
+        }
+
+        updateById(shop);
+
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
+        return Result.ok();
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
+import com.hmdp.service.IShopService;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.security.Key;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +26,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private IShopTypeService typeService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result rqurey() {
+
+
+        String Json = stringRedisTemplate.opsForValue().get("cache:shop:type");
+        if (StrUtil.isNotBlank(Json)){
+            List<ShopType> shopTypeList = JSONUtil.toList(JSONUtil.parseArray(Json), ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+
+        List<ShopType> typeList = typeService.query().orderByAsc("sort").list();
+
+        if (typeList.isEmpty()){
+            return Result.fail("TYPE IS EMPTY");
+        }
+
+        stringRedisTemplate.opsForValue().set("cache:shop:type",JSONUtil.toJsonStr(typeList));
+
+        return Result.ok(typeList);
+    }
 }
