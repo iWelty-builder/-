24e2ee28364507a8fbf330bc2644e23e6ab37f0f
master <Welty898@gmail.com>
2025-10-15 15:24:58 +0800

    使用Redis Stream 消息队列来优化异步秒杀

--- src/main/resources/seckill.lua
+++ src/main/resources/seckill.lua
@@ -8,6 +8,8 @@
 local voucherId = ARGV[1]
 -- 1.2.用户id
 local userId = ARGV[2]
+-- 1.3
+local orderId = ARGV[3]
 -- 2.数据key
 -- 2.1.库存key
 local stockKey = 'seckill:stock:'.. voucherId
@@ -28,4 +30,5 @@ end
 redis.call('incrby', stockKey, -1)
 --3.5.下单(保存用户) sadd orderkey userId
 redis.call('sadd', orderKey, userId)
+redis.call('xadd', 'stream.orders', '*', 'userId', userId, 'voucherId', voucherId, 'id', orderId)
 return 0
